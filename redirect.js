1532165c70d84ab06137bc7db74f357a42b36da4
megaofertas <eibelmm1@gmail.com>
2025-12-09 14:18:00 -0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,8 +1,1 @@
-<script>
-// Este archivo se ve como "backend" a TikTok.
-// No contiene deep links → TikTok NO lo bloquea.
 
-setTimeout(() => {
-    window.location.href = "./final.html";
-}, 10);
-</script>
